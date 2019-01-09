8dbca2a956b123ce575621bd9ffd41db14ff3ba6
ianholl <firedog1024@gmail.com>
2019-01-09 08:42:03 -0800

    adding in configuration file

---
+++ mkr10x0_iotc/configure.h
@@ -0,0 +1,20 @@
+// Azure IoT Central device information
+static char PROGMEM iotc_scopeId[] = "<replace with IoT Central scope id>";
+static char PROGMEM iotc_deviceId[] = "<replace with IoT Central device id>";
+static char PROGMEM iotc_deviceKey[] = "<replace with IoT Central device key>";
+static char PROGMEM iotConnStr[] = "<replace with IoT Hub connection string>";
+
+// Wi-Fi information
+static char PROGMEM wifi_ssid[] = "<replace with Wi-Fi SSID>";
+static char PROGMEM wifi_password[] = "<replace with Wi-Fi password>";
+
+// comment / un-comment the correct sensor type being used
+//#define SIMULATE_DHT_TYPE
+//#define DHT11_TYPE
+#define DHT22_TYPE
+
+// for DHT11/22, 
+//   VCC: 5V or 3V
+//   GND: GND
+//   DATA: 2
+int pinDHT = 2;
